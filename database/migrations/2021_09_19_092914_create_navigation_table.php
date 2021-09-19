533b7374fe8b3ae2d1faf763105331d79d354c44
Илья <yakushinilya74@gmail.com>
2021-09-19 21:02:00 +0500

    Дополнения 19.09.2021

---
+++ database/migrations/2021_09_19_092914_create_navigation_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNavigationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('navigation', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('navigation');
+    }
+}
---
+++ resources/views/basic/_sidebar.blade.php
@@ -0,0 +1,18 @@
+@forelse($sidebarMenu as $item)
+    <section>
+        <header>
+            <h1>{{$item['head']}}</h1>
+        </header>
+        <div class="body-module">
+            <ul>
+                @foreach($item['list'] as $itemList)
+                    <li>
+                        <a href="{{$itemList['route']}}">{{$itemList['head']}}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </section>
+@empty
+    No sidebar
+@endforelse
